aff6eed25a38932b8bf05b7d306a6ec9ebb45175
fanyang89 <fanyang89@outlook.com>
2024-07-08 16:23:26 +0800

    Use repr enum

--- hawkeye-common/src/lib.rs
+++ hawkeye-common/src/lib.rs
@@ -9,25 +9,21 @@ pub struct Event {
     pub event_type: EventType,
 }
 
+// check the probe with bpftrace
 #[derive(Copy, Clone)]
+#[repr(u32)]
 pub enum EventType {
-    clock_gettime,       // tracepoint:syscalls:sys_enter_clock_gettime
-    fsync,               // tracepoint:syscalls:sys_enter_fsync
-    fdatasync,           // tracepoint:syscalls:sys_enter_fdatasync
-    down_killable,       // kprobe:down_killable
-    down_read_killable,  // kprobe:down_read_killable
-    down_write_killable, // kprobe:down_write_killable
+    clock_gettime = 0,       // uprobe:/usr/lib/libc.so.6:clock_gettime
+    fsync = 1,               // uprobe:/usr/lib/libc.so.6:fsync
+    fdatasync = 2,           // uprobe:/usr/lib/libc.so.6:fdatasync
+    down_killable = 3,       // kprobe:down_killable
+    down_read_killable = 4,  // kprobe:down_read_killable
+    down_write_killable = 5, // kprobe:down_write_killable
+    posix_fadvise = 6,       // uprobe:/usr/lib/libc.so.6:posix_madvise
 }
 
-impl Into<u32> for EventType {
-    fn into(self) -> u32 {
-        match self {
-            EventType::clock_gettime => 0,
-            EventType::fsync => 1,
-            EventType::fdatasync => 2,
-            EventType::down_killable => 3,
-            EventType::down_read_killable => 4,
-            EventType::down_write_killable => 5,
-        }
+impl From<EventType> for u32 {
+    fn from(value: EventType) -> Self {
+        value as u32
     }
 }
